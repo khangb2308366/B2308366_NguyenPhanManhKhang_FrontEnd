53de3a275a604ad384abf11233e8d88d2a323cf4
Manh Khang <khangb2308366@student.ctu.edu.vn>
2026-03-23 19:14:36 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,39 @@
-import ContactBook from "@/views/ContactBook.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
+// Import các view
+import ContactBook from "@/views/ContactBook.vue";
+import AddContact from "@/views/AddContact.vue";
+import EditContact from "@/views/EditContact.vue";
+
+// Khai báo routes
 const routes = [
   {
     path: "/",
     name: "contactbook",
     component: ContactBook,
   },
+  {
+    path: "/add",
+    name: "contact.add",
+    component: AddContact,
+  },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: EditContact,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
+
+// Tạo router
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+// Export
+export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="page">
+    <h3>404 - Không tìm thấy trang</h3>
+    <router-link to="/">Quay về trang chủ</router-link>
+  </div>
+</template>
